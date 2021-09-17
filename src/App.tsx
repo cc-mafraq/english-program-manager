c463119a68e066da21bd2f42457679f4ac718d43
Jonathan Ellis <jde27@students.calvin.edu>
2021-09-18 00:05:45 +0300

    Add Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react';
 import './App.css';
+import { MenuDrawer } from './components';
 
-function App() {
-  return <div className="App">CCM English Program Manager Hello World</div>;
-}
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#002060',
+    },
+    secondary: {
+      main: '#b3e5fc',
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <MenuDrawer />
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,132 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  SvgIconTypeMap,
+  Theme,
+} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import PersonIcon from '@mui/icons-material/Person';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import CreateIcon from '@mui/icons-material/Create';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+import React, { useState, KeyboardEvent, MouseEvent } from 'react';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
+const DRAWER_WIDTH = 250;
+
+interface DrawerListItem {
+  name: string;
+  component: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string;
+  };
+}
+const iconProps = { style: { color: 'white' } };
+const drawerList: DrawerListItem[] = [
+  {
+    name: 'Home',
+    component: HomeIcon,
+  },
+  {
+    name: 'Virtual Library',
+    component: LibraryBooksIcon,
+  },
+  {
+    name: 'Student Database',
+    component: PersonIcon,
+  },
+  {
+    name: 'Class Lists',
+    component: FormatListBulletedIcon,
+  },
+  {
+    name: 'Attendance',
+    component: CreateIcon,
+  },
+  {
+    name: 'Gradebook',
+    component: EqualizerIcon,
+  },
+  {
+    name: 'Schedules',
+    component: ScheduleIcon,
+  },
+  {
+    name: 'Admin Documents',
+    component: AttachFileIcon,
+  },
+  {
+    name: 'Statistics',
+    component: AutoGraphIcon,
+  },
+];
+
+export const MenuDrawer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = (event: KeyboardEvent | MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as KeyboardEvent).key === 'Tab' ||
+        (event as KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+    setIsOpen(!isOpen);
+  };
+
+  const drawerItemList = () => (
+    <Box
+      sx={{
+        width: DRAWER_WIDTH,
+        bgcolor: 'primary.main',
+        color: 'white',
+      }}
+      role="presentation"
+    >
+      <img src="/ccm-logo.PNG" width={DRAWER_WIDTH} />
+      <List>
+        {drawerList.map((item) => (
+          <Box
+            sx={{
+              '&:hover': {
+                backgroundColor: 'primary.dark',
+                opacity: [0.9, 0.8, 0.7],
+              },
+              marginTop: '0.86vh',
+            }}
+            key={item.name}
+          >
+            <ListItem button>
+              <ListItemIcon>
+                <item.component {...iconProps} />
+              </ListItemIcon>
+              <ListItemText primary={item.name} />
+              <Divider />
+            </ListItem>
+          </Box>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <React.Fragment>
+        <Button onClick={toggleDrawer}>Menu</Button>
+        <Drawer anchor="left" open={isOpen} onClose={toggleDrawer}>
+          {drawerItemList()}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+};
