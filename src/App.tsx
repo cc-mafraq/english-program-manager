60166ad922ee6173545c9c1f7db91defb1c59049
Jonathan Ellis <jde27@students.calvin.edu>
2021-09-17 15:06:48 +0300

    Add hello world

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">CCM English Program Manager</div>;
+  return <div className="App">CCM English Program Manager Hello World</div>;
 }
 
 export default App;
