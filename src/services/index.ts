8f11db1dc9abfef1006fec723a114fa539b15d90
Jonathan Ellis <jde27@students.calvin.edu>
2021-12-08 15:50:34 +0200

    Merge

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 export * from "./fgrService";
 export * from "./filterService";
 export * from "./firebaseService";
+export * from "./loginService";
 export * from "./parsingService";
 export * from "./spreadsheetService";
 export * from "./studentDataService";
---
+++ src/services/loginService.ts
@@ -0,0 +1,37 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+export const loginWithGoogle = () => {
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      const { user } = result;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const { email } = error;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+export const logout = () => {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
