f56a5afa84ecb1b403340ff9c14153447951719c
Jonathan Ellis <jde27@students.calvin.edu>
2022-01-25 19:34:58 +0300

    Fix spreadsheet import issue

--- src/services/spreadsheetService.ts
+++ src/services/spreadsheetService.ts
@@ -9,27 +9,18 @@ export interface ValidFields {
 
 const fieldCleanRegex = /[\s)(\-#/+:,;&%]/g; // /\s|(|)|-|#|\/|+|:|,|;|&|%/g;
 
-const studentFields: ValidFields = ps.expand({
+const studentFieldsUnexpanded: ValidFields = {
   ADJ: ps.parseOrigPlacementAdjustment,
   AGEATPROGENTRY: ps.parseAge,
   AUD: ps.parseAudit,
-  "Att,Att42,Att55,Att68,Att81,Att94,Att107,Att120,Att133,Att146,Att159,Att172,Att185": ps.parseAcademicRecordAttendance,
   CERTREQUESTSDATE: ps.parseCertRequests,
-  "CORRESPONDENCE1,CORRESPONDENCE2,CORRESPONDENCE3,CORRESPONDENCE4": ps.parseCorrespondence,
   CURRENTLEVEL: ps.parseCurrentLevel,
   CURRENTSTATUS: ps.parseCurrentStatus,
-  "Cert,Cert44,Cert57,Cert70,Cert83,Cert96,Cert109,Cert122,Cert135,Cert148,Cert161,Cert174": ps.parseAcademicRecordCertificate,
   DATESENT: ps.parseClassListSentDate,
   EnglishTeacher: ps.parseEnglishTeacher,
-  "ExitSpeakingExamPF,ExitSpeakingExamPF41,ExitSpeakingExamPF54,ExitSpeakingExamPF67,ExitSpeakingExamPF80,ExitSpeakingExamPF93,ExitSpeakingExamPF106,ExitSpeakingExamPF119,ExitSpeakingExamPF132,ExitSpeakingExamPF145,ExitSpeakingExamPF158,ExitSpeakingExamPF171,ExitSpeakingExamPF184":
-    ps.parseAcademicRecordExitSpeakingExam,
-  "ExitWritingExamPF,ExitWritingExamPF40,ExitWritingExamPF53,ExitWritingExamPF66,ExitWritingExamPF79,ExitWritingExamPF92,ExitWritingExamPF105,ExitWritingExamPF118,ExitWritingExamPF131,ExitWritingExamPF144,ExitWritingExamPF157,ExitWritingExamPF170,ExitWritingExamPF183":
-    ps.parseAcademicRecordExitWritingExam,
   "FAI17,FAII17,SPI18,SPII18,FAI18,FAII18,SPI19,SPII19,FAI19,FAII19,SPI20,SPII20,FAI20,FAII20,SPI21,FAI21,FAII21,SPI22,SPII22,FAI22,FAII22":
     ps.parseInitialSession,
   FGRDATE: ps.parseFgrDate,
-  "FinalGrades,FinalGrades39,FinalGrades52,FinalGrades65,FinalGrades78,FinalGrades91,FinalGrades104,FinalGrades117,FinalGrades130,FinalGrades143,FinalGrades156,FinalGrades169,FinalGrades182":
-    ps.parseAcademicRecordFinalGrade,
   ID: ps.parseID,
   IfELTPubschlprivschlunivorrefgcamp: ps.parseEnglishTeacherLocation,
   IlliterateAR: ps.parseArabicLiteracy,
@@ -38,10 +29,6 @@ const studentFields: ValidFields = ps.expand({
   LEVELREVEALDATE: ps.parseLevelRevealDate,
   "LackofChildCare,LackofTransport,TimeConflict,IllnessorPregnancy,VisionProblems,GotaJob,Moved,GraduatedfromL5,FailedtoThriveinClsrmEnv,LackofLifeMgmSkills,LackofFamilialSupport,LackofCommitmentorMotivation,FamilyMemberorEmployerForbidFurtherStudy,COVID19PandemicRelated,Unknown":
     ps.parseDropoutReason,
-  "LevelAttended,LevelAttended34,LevelAttended47,LevelAttended60,LevelAttended73,LevelAttended86,LevelAttended99,LevelAttended112,LevelAttended125,LevelAttended138,LevelAttended151,LevelAttended164,LevelAttended177":
-    ps.parseAcademicRecordLevel,
-  "LevelAudited,LevelAudited35,LevelAudited48,LevelAudited61,LevelAudited74,LevelAudited87,LevelAudited100,LevelAudited113,LevelAudited126,LevelAudited139,LevelAudited152,LevelAudited165,LevelAudited178":
-    ps.parseAcademicRecordAudit,
   LookingforaJobDate: ps.parseLookingForJob,
   M: ps.parseGender,
   NACSWPM: ps.parseNoAnswerClassSchedule,
@@ -50,10 +37,7 @@ const studentFields: ValidFields = ps.expand({
   NCL: ps.parseNCL,
   NOTIFIED: ps.parseNotified,
   OCCUPATION: ps.parseOccupation,
-  "P,P36,P49,P62,P75,P88,P101,P114,P127,P140,P153,P166,P179,F30,F37,F50,F63,F76,F89,F102,F115,F128,F141,F154,F167,F180,WD31,WD38,WD51,WD64,WD77,WD90,WD103,WD116,WD129,WD142,WD155,WD168,WD181":
-    ps.parseAcademicRecordResult,
   PENDINGPLCM: ps.parsePendingPlacement,
-  "PHONE,PHONE20,PHONE21,PHONE22,PHONE23,PHONE24,PHONE25": ps.parsePhone,
   PHOTOCONTACT: ps.parsePhotoContact,
   PLCMCONFDATE: ps.parsePlacementConfDate,
   PLCMLVL: ps.parseOrigPlacementLevel,
@@ -62,21 +46,46 @@ const studentFields: ValidFields = ps.expand({
   SECSOFFERED: ps.parseSectionsOffered,
   SENTTEXTEDCL: ps.parseClassListSent,
   SPKG: ps.parseOrigPlacementSpeaking,
-  "Ses,Ses33,Ses46,Ses59,Ses72,Ses85,Ses98,Ses111,Ses124,Ses137,Ses150,Ses163,Ses176": ps.parseAcademicRecordSession,
-  "TeacherComments,TeacherComments43,TeacherComments56,TeacherComments69,TeacherComments82,TeacherComments95,TeacherComments108,TeacherComments121,TeacherComments134,TeacherComments147,TeacherComments160,TeacherComments173,TeacherComments186":
-    ps.parseAcademicRecordTeacherComments,
   TeacherorProfessor: ps.parseTeacher,
   TeachingSubjectAreas: ps.parseTeachingSubjectAreas,
   TutorClubDetails: ps.parseZoomTutor,
   TutorDate: ps.parseLiteracyTutor,
+  WA: ps.parseWaPrimPhone,
   WABCSAR: ps.parseWABroadcastSAR,
   "WABCSARL35W,WABCSAREngTchrs,WABCSAREngTchrsL35,WABCPhotography,WABCGerman": ps.parseWABroadcasts,
-  WAPRIMPHONE: ps.parseWaPrimPhone,
   WASTATUS: ps.parseWAStatus,
   WDDATE: ps.parseWithdrawDate,
   WRTG: ps.parseOrigPlacementWriting,
   Y: ps.parseInviteTag,
-});
+};
+const maxAcademicRecordColumnNum = 200;
+studentFieldsUnexpanded[ps.generateKeys("Att", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordAttendance;
+studentFieldsUnexpanded[ps.generateKeys("CORRESPONDENCE", 10)] = ps.parseCorrespondence;
+studentFieldsUnexpanded[ps.generateKeys("Cert", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordCertificate;
+studentFieldsUnexpanded[ps.generateKeys("ExitSpeakingExamPF", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordExitSpeakingExam;
+studentFieldsUnexpanded[ps.generateKeys("ExitWritingExamPF", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordExitWritingExam;
+studentFieldsUnexpanded[ps.generateKeys("FinalGrades", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordFinalGrade;
+studentFieldsUnexpanded[ps.generateKeys("LevelAttended", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordLevel;
+studentFieldsUnexpanded[ps.generateKeys("LevelAudited", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordAudit;
+studentFieldsUnexpanded[ps.generateKeys("P", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordResult;
+studentFieldsUnexpanded[ps.generateKeys("F", maxAcademicRecordColumnNum, true)] =
+  ps.parseAcademicRecordResult;
+studentFieldsUnexpanded[ps.generateKeys("WD", maxAcademicRecordColumnNum, true)] =
+  ps.parseAcademicRecordResult;
+studentFieldsUnexpanded[ps.generateKeys("PHONE", 50)] = ps.parsePhone;
+studentFieldsUnexpanded[ps.generateKeys("Ses", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordSession;
+studentFieldsUnexpanded[ps.generateKeys("TeacherComments", maxAcademicRecordColumnNum)] =
+  ps.parseAcademicRecordTeacherComments;
+const studentFields = ps.expand(studentFieldsUnexpanded);
 
 export const spreadsheetToStudentList = (csvString: string): Student[] => {
   // Remove junk and title rows from Excel export to CSV
@@ -101,15 +110,14 @@ export const spreadsheetToStudentList = (csvString: string): Student[] => {
     if (fields) {
       fields.forEach((field) => {
         const value = String(object[field]);
+        if (student.epId === 19133 && field === "P") {
+          console.log(value);
+        }
         const fieldClean = replace(field, fieldCleanRegex, "");
         if (fieldClean in studentFields) {
           studentFields[fieldClean as keyof ValidFields](field, value, student);
         }
       });
-      // student.phone.primaryPhone = indexOf(
-      //   map(student.phone.phoneNumbers, "number"),
-      //   student.phone.primaryPhone,
-      // );
       student.epId !== 0 && students.push(student);
     }
   });
