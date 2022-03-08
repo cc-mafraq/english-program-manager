9cbd3fc9e8fff551959922b7f8b5e6173124c649
Jonathan Ellis <jde27@students.calvin.edu>
2022-03-08 16:10:52 +0300

    test with failing test

--- src/tests/parsingService.spec.ts
+++ src/tests/parsingService.spec.ts
@@ -47,7 +47,7 @@ describe("test expand", () => {
 describe("parses names", () => {
   it("parses English name", () => {
     parseEnglishName("", "Jon Ellis", student);
-    expect(student.name.english).toEqual("Jon Ellis");
+    expect(student.name.english).toEqual("Jon Elli");
   });
   it("parses Arabic name", () => {
     parseArabicName("", "جون إليس", student);
