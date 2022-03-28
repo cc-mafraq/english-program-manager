a5970ce9d4cceb93121a4004486261589f82c05b
Jonathan Ellis <jde27@students.calvin.edu>
2022-03-28 12:02:41 +0300

    add firebase dev env

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseProdConfig = {
   apiKey: "AIzaSyDyoV4oFFFbXcSjUPeKbqpJrM1a1X12m_8",
   appId: "1:1006817124710:web:9825e70910a29b74f668a7",
   authDomain: "ccm-english.firebaseapp.com",
@@ -17,7 +17,19 @@ const firebaseConfig = {
   storageBucket: "ccm-english.appspot.com",
 };
 
+const firebaseDevConfig = {
+  apiKey: "AIzaSyBWYcxes1Ozhp8XANNegcAQOFpvSDdab2k",
+  appId: "1:563679619638:web:45465cd26144608fc3c697",
+  authDomain: "ccm-english-dev.firebaseapp.com",
+  measurementId: "G-TT5H32705E",
+  messagingSenderId: "563679619638",
+  projectId: "ccm-english-dev",
+  storageBucket: "ccm-english-dev.appspot.com",
+};
+
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(
+  window.location.hostname.includes("ccm-english.") ? firebaseProdConfig : firebaseDevConfig,
+);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
