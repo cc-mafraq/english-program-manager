b174a4d752d360f2f52574f8dd4000882b294b14
Jonathan Ellis <jde27@students.calvin.edu>
2022-06-23 11:01:27 -0400

    Add more styling to filter

--- src/components/StudentDatabaseToolbar/Filter/FilterDrawer.tsx
+++ src/components/StudentDatabaseToolbar/Filter/FilterDrawer.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Drawer, Typography } from "@mui/material";
+import { Box, Button, Drawer, Typography, useTheme } from "@mui/material";
 import { map, sortBy, uniq } from "lodash";
 import React, { useContext, useMemo } from "react";
 import { FilterCheckbox } from "..";
@@ -26,6 +26,7 @@ export const FilterDrawer: React.FC<FilterDrawerProps> = ({ anchorEl, handleClos
     appDispatch,
   } = useContext(AppContext);
   const { popoverColor } = useColors();
+  const theme = useTheme();
 
   const filterFields: FilterField[] = useMemo(() => {
     return [
@@ -57,12 +58,25 @@ export const FilterDrawer: React.FC<FilterDrawerProps> = ({ anchorEl, handleClos
       <Typography fontWeight="bold" marginLeft={1} marginTop={1} variant="h5">
         Filter Students
       </Typography>
-      <Button onClick={handleClearFilters}>Clear Filters</Button>
+      <Button
+        color={theme.palette.mode === "light" ? "secondary" : undefined}
+        onClick={handleClearFilters}
+        sx={{
+          borderRadius: 8,
+          marginBottom: -3,
+          marginLeft: theme.palette.mode === "light" ? "3%" : 0.5,
+          marginTop: 1,
+          width: theme.palette.mode === "light" ? "75%" : "60%",
+        }}
+        variant={theme.palette.mode === "light" ? "contained" : undefined}
+      >
+        Clear Filters
+      </Button>
       <Box>
         {map(filterFields, (field, i) => {
           return (
-            <Box key={`filter-field-${i}`} margin={1}>
-              <Typography fontWeight="bold" marginBottom={0.5} width="100%">
+            <Box key={`filter-field-${i}`} margin={1} marginTop={5}>
+              <Typography fontSize={18} fontWeight="bold" marginBottom={0.5} width="100%">
                 {field.name}
               </Typography>
               {map(field.values ? field.values : sortBy(uniq(map(students, field.path))), (val) => {
