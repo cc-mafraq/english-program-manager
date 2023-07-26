0a7d96f5408dece752fb06ebb29cd26d24b0f4dd
Jon Ellis <jdellis818@gmail.com>
2023-07-26 15:31:10 +0300

    separate into component

--- src/pages/StudentDatabasePage.tsx
+++ src/pages/StudentDatabasePage.tsx
@@ -1,11 +1,10 @@
-import { FilterListOutlined, SearchOutlined } from "@mui/icons-material";
-import { Box, Typography, useTheme } from "@mui/material";
 import { filter as _filter, isArray, isEmpty } from "lodash";
 import React, { useCallback, useMemo, useRef } from "react";
 import {
   FinalGradeReportDialog,
   Loading,
   MenuBar,
+  StudentDatabaseHome,
   StudentDatabaseToolbar,
   StudentFormDialog,
   StudentList,
@@ -26,8 +25,6 @@ export const StudentDatabasePage = () => {
   });
 
   const menuRef = useRef<HTMLDivElement>(null);
-
-  const theme = useTheme();
 
   const handleStudentDialogOpen = useCallback(() => {
     setStudentDialogOpen(true);
@@ -59,54 +56,7 @@ export const StudentDatabasePage = () => {
         handleStudentDialogOpen={handleStudentDialogOpen}
         searchString={searchString}
       />
-      {shouldEmpty && (
-        <Box color="white" position="relative" textAlign="center">
-          <Box
-            sx={{
-              fontVariant: "small-caps",
-              left: "50%",
-              position: "absolute",
-              textShadow: "1.5px 1.5px #000000",
-              top: "20vh",
-              transform: "translate(-50%, -50%)",
-              width: "100%",
-            }}
-          >
-            <Typography letterSpacing="0.5vw" variant="h3">
-              Welcome to the CCM Student Database!
-            </Typography>
-            <Typography letterSpacing="0.5vw" marginTop="1vh" variant="h3">
-              <SearchOutlined
-                fontSize="large"
-                sx={{
-                  color: theme.palette.primary.main,
-                  filter: "drop-shadow(0.75px 0.75px #000000)",
-                  marginRight: "10px",
-                  marginTop: "20px",
-                }}
-              />
-              search or{" "}
-              <FilterListOutlined
-                fontSize="large"
-                sx={{
-                  color: theme.palette.primary.main,
-                  filter: "drop-shadow(0.75px 0.75px #000000)",
-                  marginRight: "10px",
-                  marginTop: "5px",
-                }}
-              />
-              filter to get started.
-            </Typography>
-          </Box>
-          <img
-            alt="Mafraq Art"
-            src={
-              theme.palette.mode === "dark" ? "./assets/mafraq-art-wide-dark.jpg" : "./assets/mafraq-art-wide.jpg"
-            }
-            width="100%"
-          />
-        </Box>
-      )}
+      {shouldEmpty && <StudentDatabaseHome />}
       <Loading />
       <StudentList
         filteredStudents={_filter(shouldEmpty ? [] : filteredStudents, (student) => {
