ab96c3df71cf66d10fb5bcd4708828690cfbc728
Jon Ellis <jdellis818@gmail.com>
2023-07-26 15:23:38 +0300

    Add empty start page

--- src/pages/StudentDatabasePage.tsx
+++ src/pages/StudentDatabasePage.tsx
@@ -1,5 +1,7 @@
-import { filter as _filter, isArray } from "lodash";
-import React, { useCallback, useRef } from "react";
+import { FilterListOutlined, SearchOutlined } from "@mui/icons-material";
+import { Box, Typography, useTheme } from "@mui/material";
+import { filter as _filter, isArray, isEmpty } from "lodash";
+import React, { useCallback, useMemo, useRef } from "react";
 import {
   FinalGradeReportDialog,
   Loading,
@@ -25,6 +27,8 @@ export const StudentDatabasePage = () => {
 
   const menuRef = useRef<HTMLDivElement>(null);
 
+  const theme = useTheme();
+
   const handleStudentDialogOpen = useCallback(() => {
     setStudentDialogOpen(true);
   }, [setStudentDialogOpen]);
@@ -42,20 +46,70 @@ export const StudentDatabasePage = () => {
     sortFn: sortStudents,
   });
 
+  const shouldEmpty = useMemo(() => {
+    return isEmpty(searchString) && isEmpty(filter);
+  }, [filter, searchString]);
+
   return (
     <>
       <MenuBar innerRef={menuRef} pageName="Student Database" />
       <StudentDatabaseToolbar
-        filteredStudents={_filter(filteredStudents, (student) => {
-          return isArray(student.placement);
-        })}
+        filteredStudents={shouldEmpty ? [] : filteredStudents}
         handleSearchStringChange={handleSearchStringChange}
         handleStudentDialogOpen={handleStudentDialogOpen}
         searchString={searchString}
       />
+      {shouldEmpty && (
+        <Box color="white" position="relative" textAlign="center">
+          <Box
+            sx={{
+              fontVariant: "small-caps",
+              left: "50%",
+              position: "absolute",
+              textShadow: "1.5px 1.5px #000000",
+              top: "20vh",
+              transform: "translate(-50%, -50%)",
+              width: "100%",
+            }}
+          >
+            <Typography letterSpacing="0.5vw" variant="h3">
+              Welcome to the CCM Student Database!
+            </Typography>
+            <Typography letterSpacing="0.5vw" marginTop="1vh" variant="h3">
+              <SearchOutlined
+                fontSize="large"
+                sx={{
+                  color: theme.palette.primary.main,
+                  filter: "drop-shadow(0.75px 0.75px #000000)",
+                  marginRight: "10px",
+                  marginTop: "20px",
+                }}
+              />
+              search or{" "}
+              <FilterListOutlined
+                fontSize="large"
+                sx={{
+                  color: theme.palette.primary.main,
+                  filter: "drop-shadow(0.75px 0.75px #000000)",
+                  marginRight: "10px",
+                  marginTop: "5px",
+                }}
+              />
+              filter to get started.
+            </Typography>
+          </Box>
+          <img
+            alt="Mafraq Art"
+            src={
+              theme.palette.mode === "dark" ? "./assets/mafraq-art-wide-dark.jpg" : "./assets/mafraq-art-wide.jpg"
+            }
+            width="100%"
+          />
+        </Box>
+      )}
       <Loading />
       <StudentList
-        filteredStudents={_filter(filteredStudents, (student) => {
+        filteredStudents={_filter(shouldEmpty ? [] : filteredStudents, (student) => {
           return isArray(student.placement);
         })}
         handleStudentDialogOpen={handleStudentDialogOpen}
