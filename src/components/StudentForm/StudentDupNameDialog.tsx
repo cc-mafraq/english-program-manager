7fddcb73893998d0d1a3c07ef2938853400da3b1
Jonathan Ellis <jdellis818@gmail.com>
2025-05-15 16:49:53 +0300

    Add duplicate student popup

---
+++ src/components/StudentForm/StudentDupNameDialog.tsx
@@ -0,0 +1,74 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import React from "react";
+import { DialogProps, useColors } from "../../hooks";
+import { Student } from "../../interfaces";
+
+interface StudentDupNameDialogProps {
+  data: Student;
+  handleDialogClose: DialogProps["onClose"];
+  handleSearchStringChange: (newString: string) => void;
+  handleStudentDialogClose: () => void;
+  matchedStudentId?: Student["epId"];
+  onSubmit: (data: Student) => void;
+  open: boolean;
+}
+
+export const StudentDupNameDialog: React.FC<StudentDupNameDialogProps> = ({
+  open,
+  handleDialogClose,
+  handleStudentDialogClose,
+  handleSearchStringChange,
+  onSubmit,
+  data,
+  matchedStudentId,
+}) => {
+  const { popoverColor } = useColors();
+
+  return (
+    <Dialog
+      disableEscapeKeyDown
+      onClose={(_, reason) => {
+        return handleDialogClose(reason);
+      }}
+      open={open}
+      PaperProps={{
+        style: {
+          backgroundColor: popoverColor,
+          overflowX: "hidden",
+        },
+      }}
+    >
+      <DialogTitle>Are you sure you want to add this entry?</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          The name you entered is already registered in the student database. If you are sure this is a different
+          person, then continue. Otherwise, click &quot;No&quot; and edit the older entry.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            handleDialogClose("submit");
+            onSubmit(data);
+          }}
+        >
+          Yes
+        </Button>
+        <Button
+          autoFocus
+          onClick={() => {
+            handleDialogClose("submit");
+            handleStudentDialogClose();
+            matchedStudentId && handleSearchStringChange(matchedStudentId.toString());
+          }}
+        >
+          No
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+StudentDupNameDialog.defaultProps = {
+  matchedStudentId: undefined,
+};
--- src/components/StudentForm/index.ts
+++ src/components/StudentForm/index.ts
@@ -1,3 +1,4 @@
 export * from "./FormSections";
+export * from "./StudentDupNameDialog";
 export * from "./StudentForm";
 export * from "./StudentFormDialog";
